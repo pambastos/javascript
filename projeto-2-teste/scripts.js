b016c3ef34dcc499a5278de0648977912f43f9db
pam <panpbastos@gmail.com>
2022-03-11 15:52:50 -0300

    alterando

---
+++ projeto-2-teste/scripts.js
@@ -0,0 +1,11 @@
+let amor = true;
+let respeito = true;
+
+function compareTrue(amor, respeito) {
+  if (amor && respeito === true) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+
+}
