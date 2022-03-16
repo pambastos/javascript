6038bec77c4ac7d927c91efdc681615f97488d88
pam <panpbastos@gmail.com>
2022-03-16 16:08:24 -0300

    aprendendo-a-usar-os-gets

--- trybepage/scripts.js
+++ trybepage/scripts.js
@@ -1,1 +1,7 @@
-document.getElementById("teste")
+document.getElementsByClassName("cor")[0].innerText = "Aula sobre tudo";
+document.getElementById("teste").innerText = "Conteúdo alterado";
+document.getElementsByTagName("span")[0].innerText = "Conteúdo alterado pela tag";
+
+
+
+
