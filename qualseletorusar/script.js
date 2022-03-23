e1d1b3cd13664112152b30ea67a5fd4e9c7792c3
pam <panpbastos@gmail.com>
2022-03-23 19:29:56 -0300

    alterando

---
+++ qualseletorusar/script.js
@@ -0,0 +1,19 @@
+const urgente1 = document.getElementsByTagName('h3')[0].style.backgroundColor = 'green' ;
+const urgente2 = document.getElementsByTagName('h3')[1].style.backgroundColor = 'green' ;
+const naourgente1 = document.getElementsByTagName('h3')[2].style.backgroundColor = 'green' ;
+const naourgente2 = document.getElementsByTagName('h3')[3].style.backgroundColor = 'green' ;
+
+
+const barrinha1 = document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'yellow' ;
+const barrinha2 = document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow' ;
+
+
+const conteudourgente1 = document.getElementsByTagName('div')[0].style.backgroundColor = 'blue' ;
+const conteudourgente2 = document.getElementsByTagName('div')[1].style.backgroundColor = 'blue' ;
+const conteudourgente3 = document.getElementsByTagName('div')[2].style.backgroundColor = 'blue' ;
+const conteudourgente4 = document.getElementsByTagName('div')[3].style.backgroundColor = 'blue' ;
+
+
+const fundo = document.getElementById('container').style.backgroundColor = 'white' ;
+  
+  
