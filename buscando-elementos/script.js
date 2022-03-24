06d611232d8e6079cbb884a58c344d436f823f7d
pam <panpbastos@gmail.com>
2022-03-24 01:46:08 -0300

    alterando

--- buscando-elementos/script.js
+++ buscando-elementos/script.js
@@ -2,6 +2,32 @@ let elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
 
 elementoOndeVoceEsta.parentElement.style.color = 'blue' ;
 
+elementoOndeVoceEsta.nextElementSibling.innerText = 'Olá, mundo!';
+
+let pai = document.getElementById('pai');
+pai.firstElementChild;
+
+
+elementoOndeVoceEsta.previousElementSibling;
+
+elementoOndeVoceEsta.nextSibling;
+
+elementoOndeVoceEsta.nextElementSibling;
+
+pai.lastElementChild.previousElementSibling;
+
+
+
+var irmaoDoElementoOndeVoceEsta = document.createElement('section');
+irmaoDoElementoOndeVoceEsta.id = "irmaoDoElementoOndeVoceEsta";
+pai.appendChild(irmaoDoElementoOndeVoceEsta);
+
+
+
+
+
+
+
 
 
 
