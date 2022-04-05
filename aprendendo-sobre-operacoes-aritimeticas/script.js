b616a9e59d0628f8f389ff941bd79132fbeeec01
pam <panpbastos@gmail.com>
2022-04-04 23:55:38 -0300

    finalizando-exercicio-sobre-operadores-aritimeticos

---
+++ aprendendo-sobre-operacoes-aritimeticas/script.js
@@ -0,0 +1,34 @@
+let patientId = 50;
+
+let isEnrolled = true;
+
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+console.log(typeof patientAge);
+
+
+
+// ATIVIDADES
+
+// 1
+const base = 5;
+const heigth = 8;
+
+
+// 2
+const area = base * heigth
+console.log(area);
+
+// 3
+const perimeter = 5 + 5 + 8 + 8;
+console.log(perimeter);
