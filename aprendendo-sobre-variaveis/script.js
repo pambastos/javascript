9ef6ebbb86effb9a96ef31e95683dc52aab4d33c
pam <panpbastos@gmail.com>
2022-04-04 23:27:42 -0300

    finalizando-exercicio-sobre-variaveis

---
+++ aprendendo-sobre-variaveis/script.js
@@ -0,0 +1,36 @@
+let age = 27; 
+// declaração de variável
+age = 30;
+// reatribuição de variável
+console.log(age);
+// imprimir valores no coderunner
+
+let nome = "Pâmela"
+console.log(nome);
+// declaração de variável tipo string
+
+const cabelo = loiro;
+// constantes não podem ser reatribuídas
+
+
+// ATIVIDADES
+
+// 1,2,4,4
+const myName = "Pâmela Walsh";
+const birthCity = "São Gonçalo";
+let birthYear = 1995;
+console.log(myName , birthCity, birthYear);
+
+// 5,
+birthYear = 2030;
+console.log(birthYear);
+
+// 6
+birthCity = "Rio de Janeiro";
+birthCity = "São gonçalo";
+console.log(birthCity);
+
+
+
+
+
